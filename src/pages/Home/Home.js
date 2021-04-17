8eeef24e5110a2cd2701d8f541bc4c25b40695ce
lucasregalado@hotmail.com <lucas_onetree@medable.com>
2021-04-17 19:41:51 -0300

    Add - logo in header

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Home.scss';
+import logo from '../../assets/images/logo.svg';
 
 function Home() {
 
   return (
-    <div className="Items">
-        <h1>I'm here</h1>
+    <div className="Home">
+      <header className="header_home">
+      <img src={logo} alt="Mercado Libre logo" />
+      </header>  
     </div>
   );
 };
