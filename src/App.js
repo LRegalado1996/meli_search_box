704b70b902654d39ce4592518c589d8a47c1e770
lucasregalado@hotmail.com <lucas_onetree@medable.com>
2021-04-16 23:04:53 -0300

    Create - item page with react route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./services/history";
+import { Items } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/items" component={ Items } />
+        <Route path="/" component={ Items } />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Items/Items.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Items() {
+
+  return (
+    <div className="Items">
+        <h1>I'm here</h1>
+    </div>
+  );
+};
+
+export { Items };
