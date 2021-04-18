416cce93dae031d9fce2db883739192b6b8900aa
lucasregalado@hotmail.com <lucas_onetree@medable.com>
2021-04-18 18:14:59 -0300

    Create - view of articles list

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -5,10 +5,39 @@ const Items = ({
   articles,
   onClikArticle
 }) => {
+
+  const renderArticle = (article) => {
+    const { thumbnail, title, price, address } = article;
+    
+    return (
+      <div className="item">
+
+        <div className="container image">
+          <img src={ thumbnail } alt="Mercado Libre logo" />
+        </div>
+
+        <div className="container desciption">
+          <p className="price">$ { price }</p>
+          <h3>{ title }</h3>
+        </div>
+
+        <div className="container address">
+          <p>
+            { address.city_name }
+            <br />
+            { address.state_name }
+          </p>
+        </div>
+
+      </div>
+    )
+
+  }
+
   return (
     <div className="Items">
-      <h1>Articles</h1>
-  </div>
+      { articles.map(article => renderArticle(article) ) }
+    </div>
   );
 }
 export { Items };
